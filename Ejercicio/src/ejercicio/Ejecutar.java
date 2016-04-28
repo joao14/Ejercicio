f192b5e2691073feda09fd8d00c2a8bedef77113
Alex Merino Romero <barcealex1014@hotmail.com>
2016-04-28 14:35:44 -0500

    Ingnore file.

--- Ejercicio/src/ejercicio/Ejecutar.java
+++ Ejercicio/src/ejercicio/Ejecutar.java
@@ -5,7 +5,10 @@ public class Ejecutar {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Ejecutar servicvios");
-		System.out.println("Localizar");    
+		System.out.println("Localizar");
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Ejercicio de productividad" + i);
+		}
 
 	}
 
