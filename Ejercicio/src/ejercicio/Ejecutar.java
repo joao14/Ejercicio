1032c2fe76763a6408b16038c0afbfe5b58222f2
Alex Merino Romero <barcealex1014@hotmail.com>
2016-04-28 14:16:06 -0500

    Primeros cambios del ejercicio.

---
+++ Ejercicio/src/ejercicio/Ejecutar.java
@@ -0,0 +1,12 @@
+package ejercicio;
+
+public class Ejecutar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Ejecutar servicvios");
+		System.out.println("Localizar");    
+
+	}
+
+}
